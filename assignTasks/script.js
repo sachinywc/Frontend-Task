a6784a1a0c8eec57464fc0642c003b8e0a9d49c0
Sachin Pandey <yogathing0@gmail.com>
2025-08-20 11:30:05 +0545

    first-commit

--- assignTasks/script.js
+++ assignTasks/script.js
@@ -2,7 +2,7 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
-    if(inputBox.value === '') {
+    if (inputBox.value === '') {
         alert("Please enter a task");
     } else {
         let li = document.createElement("li");
@@ -17,11 +17,10 @@ function addTask() {
 }
 
 listContainer.addEventListener("click", function (e) {
-    if(e.target.tagName === "LI"){
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
@@ -30,7 +29,9 @@ listContainer.addEventListener("click", function (e) {
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
+
 function showTasks() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
+
 showTasks();
---
+++ duMmy/text.js
@@ -0,0 +1,49 @@
+// function addTasks(a, b) {
+//     return a + b;
+// }
+//
+// console.log(addTasks(5, 3));
+
+
+// function addTask(a, b) {
+//     if (a === b) {
+//         return a + b;
+//
+//     } else if (a === b) {
+//         return a - b;
+//     } else {
+//         return a * b;
+//     }
+// }
+
+
+function checkNum(num) {
+    if (num < 12) {
+        return "You are child";
+    } else if (num < 30) {
+        return "You are young";
+    } else {
+        return "You are adult";
+    }
+}
+
+console.log(checkNum(30));
+
+// function addTask(a,b) {
+//     return a + b;
+// }
+//
+// console.log(addTask(1,2));
+
+// function removeTask(a,b) {
+//     return a + b;
+// }
+//
+// console.log(removeTask(4,4));
+
+
+
+
+
+
+
